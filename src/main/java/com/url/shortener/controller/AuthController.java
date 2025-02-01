0575dcb4cce8f47abc66207bec4b60b960a6fa46
Shivam kumar <1306shivam@gmail.com>
2025-02-01 20:17:27 +0530

    added ClickAnalytics

--- src/main/java/com/url/shortener/controller/AuthController.java
+++ src/main/java/com/url/shortener/controller/AuthController.java
@@ -5,7 +5,6 @@ import com.url.shortener.dtos.RegisterRequest;
 import com.url.shortener.entity.User;
 import com.url.shortener.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class AuthController {
 
-    @Autowired
+
     private UserService userService;
 
     @PostMapping("/public/register")
--- src/main/java/com/url/shortener/service/UserService.java
+++ src/main/java/com/url/shortener/service/UserService.java
@@ -7,11 +7,11 @@ import com.url.shortener.security_jwt.JwtAuthenticationResponse;
 import com.url.shortener.security_jwt.JwtUtils;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -49,5 +49,11 @@ public class UserService {
         String jwt = jwtUtils.generateToken(userDetails);
 
         return new JwtAuthenticationResponse(jwt);
+    }
+
+    public User findByUsername(String name) {
+        return userRepository.findByUsername(name).orElseThrow(
+                () -> new UsernameNotFoundException("User not found with username: "+name)
+        );
     }
 }
